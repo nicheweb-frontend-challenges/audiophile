5e3ded2592b29aef0e1dd23293e2b36b95a189fa
nicheweb <norgerojascerulia@gmail.com>
2024-12-29 21:34:02 +0100

    Install and configure fonts, add reset and typography styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./assets/styles/main.scss";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <h1>Hello World!</h1>
+    </div>
+  );
 }
 
 export default App;
