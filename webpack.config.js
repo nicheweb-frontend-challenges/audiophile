ccb46cbb8726e98bd63d13be7a20407804b1a166
nicheweb <norgerojascerulia@gmail.com>
2025-02-16 17:44:05 +0100

    webpack again 2

--- webpack.config.js
+++ webpack.config.js
@@ -119,7 +119,7 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: "public", to: "" }, // Copies everything from public to dist
+        { from: "public/assets/images", to: "assets/images" }, // Copies everything from public to dist
       ],
     }),
     new CleanWebpackPlugin(),
