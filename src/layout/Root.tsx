3f97b3ebabcd0d9dad09bfccff70caa7d86033fb
nicheweb <norgerojascerulia@gmail.com>
2025-02-16 19:56:49 +0100

    Change root styling

--- src/layout/Root.tsx
+++ src/layout/Root.tsx
@@ -4,19 +4,20 @@ import BestGearSection from "./BestGearSection";
 import Footer from "./Footer";
 import ScrollToTop from "../components/ScrollToTop";
 import Menu from "../components/menu/Menu";
+import * as styles from "./Root.module.scss";
 
 const RootLayout = () => {
   const { pathname } = useLocation();
 
   return (
-    <>
+    <div className={styles.root}>
       <ScrollToTop />
       <MainHeader />
       <Menu />
       <Outlet />
       {pathname !== "/checkout" && <BestGearSection />}
       <Footer />
-    </>
+    </div>
   );
 };
 export default RootLayout;
