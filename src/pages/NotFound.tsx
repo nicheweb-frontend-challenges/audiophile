a9ba84f2b5ad0298ae27ddc6d32e8671d896a280
nicheweb <norgerojascerulia@gmail.com>
2025-02-26 14:41:30 +0100

    Fix NoFoundPage -2

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,18 +1,20 @@
 import MainHeader from "../layout/MainHeader";
+import Footer from "../layout/Footer";
 import * as styles from "./NotFound.module.scss";
 
 const NotFoundPage = () => {
   return (
-    <>
-      <div className={styles.errorPage__headerBacground}></div>
-      <div className={styles.errorPage}>
-        <div className={styles.errorPage__container}>
+    <div className={styles.errorPage}>
+      <MainHeader />
+      <main className={styles.errorPage__main}>
+        <div className={styles.errorPage__main__container}>
           <img src="/assets/images/error.png" alt="Error 404 image" />
           <h1>An error ocurred!</h1>
           <p>Could not find this page!</p>
         </div>
-      </div>
-    </>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
